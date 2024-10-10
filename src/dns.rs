7851b728e45078b9b0a8e5b708786dc216730409
Claus Guttesen <kometen@gmail.com>
2024-10-10 20:00:31 +0200

    Name-lookup works
    It will stop if nameresolution fails.

--- src/dns.rs
+++ src/dns.rs
@@ -1,15 +1,23 @@
 pub mod dns {
 
     use crate::{Environment, Vault};
+    use hickory_resolver::error::ResolveError;
+    use hickory_resolver::system_conf::read_system_conf;
+    use hickory_resolver::AsyncResolver;
 
-    pub fn check_dns(vault: &Vault, env: &Environment) -> Result<(), Box<dyn std::error::Error>> {
-        use trust_dns_resolver::{
-            config::{ResolverConfig, ResolverOpts},
-            AsyncResolver,
-        };
+    pub async fn check_dns(
+        vault: &Vault,
+        env: &Environment,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let (config, opts) = read_system_conf().map_err(|e| ResolveError::from(e))?;
+        let resolver = AsyncResolver::tokio(config, opts);
+        let hostname = format!("{}.{}.", &vault.host, &env.domain);
+        let response = resolver.lookup_ip(hostname).await?;
 
-        let hostname = format!("{}.{}", &vault.host, &env.domain);
-        let resolver = AsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default());
+        if response.iter().next().is_none() {
+            return Err("No addresses returned".into());
+        }
+
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use backup_db::{perform_backup, Compression, Environment, FileSystem, Vault};
+use backup_db::{check_dns, perform_backup, Compression, Environment, FileSystem, Vault};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -6,6 +6,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let env = Environment::new()?;
     let fs = FileSystem::new(&compression)?;
     let vault = Vault::new().await?;
+    check_dns(&vault, &env).await?;
     perform_backup(&compression, &env, &fs, &vault).await?;
 
     Ok(())
